543a0ae599611cd7fa9d2297e6bb896972214a16
bengris32 <bengris32@protonmail.ch>
2022-09-13 20:40:38 +0100

    RMP6768: Shim missing audio symbol
    E vndksupport: Could not load /vendor/lib/hw/audio.primary.mt6768.so from default namespace: dlopen failed: cannot locate symbol "_ZN7android13TypeConverterINS_13DefaultTraitsI14audio_format_tEEE6mTableE" referenced by "/vendor/lib/hw/audio.primary.mt6768.so"....
    
    - The required conversion mTable [1] was removed in VNDK31, add it back.
    
    https://cs.android.com/android/_/android/platform/frameworks/av/+/dc6be0d5754e65ffa07fe8ef04ca4d28c2c0268b
    
    Change-Id: If9efebdc59b3f5f877b05aaaf3e773d3e6fb7c74
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- vndk/Android.mk
+++ vndk/Android.mk
@@ -1,19 +1,6 @@
 LOCAL_PATH := prebuilts/vndk
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := libutils-v32
-LOCAL_MULTILIB := both
-LOCAL_SRC_FILES_arm := v32/arm64/arch-arm-armv8-a/shared/vndk-sp/libutils.so
-LOCAL_SRC_FILES_arm64 := v32/arm64/arch-arm64-armv8-a/shared/vndk-sp/libutils.so
-LOCAL_MODULE_SUFFIX := .so
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MODULE_TARGET_ARCH := arm arm64
-LOCAL_MODULE_TAGS := optional
-LOCAL_CHECK_ELF_FILES := false
-LOCAL_VENDOR_MODULE := true
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE := libutils-v30
 LOCAL_MULTILIB := both
 LOCAL_SRC_FILES_arm := v30/arm64/arch-arm-armv8-a/shared/vndk-sp/libutils.so
@@ -27,10 +14,10 @@ LOCAL_VENDOR_MODULE := true
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := libmedia_helper-v30
+LOCAL_MODULE := libutils-v32
 LOCAL_MULTILIB := both
-LOCAL_SRC_FILES_arm := v30/arm64/arch-arm-armv8-a/shared/vndk-core/libmedia_helper.so
-LOCAL_SRC_FILES_arm64 := v30/arm64/arch-arm64-armv8-a/shared/vndk-core/libmedia_helper.so
+LOCAL_SRC_FILES_arm := v32/arm64/arch-arm-armv8-a/shared/vndk-sp/libutils.so
+LOCAL_SRC_FILES_arm64 := v32/arm64/arch-arm64-armv8-a/shared/vndk-sp/libutils.so
 LOCAL_MODULE_SUFFIX := .so
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_MODULE_TARGET_ARCH := arm arm64
